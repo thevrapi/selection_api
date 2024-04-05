1d72a9693b4af1059373769ae811af5c6609ad49
The Vrapi <thevrapi@gmail.com>
2024-04-06 01:35:22 +0200

    some work done

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export function selRange(): Range;
+export function selContainer(): HTMLElement;
+export function isInSel(nodeName:string, attributes:Object<any>): boolean;
---
+++ index.js
@@ -0,0 +1,3 @@
+import {isInSel, selContainer, selRange} from './lib/util.js';
+
+export {isInSel, selContainer, selRange};
